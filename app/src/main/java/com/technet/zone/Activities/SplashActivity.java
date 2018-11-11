8e2e19932842177379b6988ed5f3c76d65130413
Atik Shakur <atik.shakur98@gmail.com>
2018-11-11 21:29:13 +0600

    Splash Screen added

---
+++ app/src/main/java/com/technet/zone/Activities/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.technet.zone.Activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.technet.zone.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 1000);
+    }
+}
